755fc1b1ae261c1fe4cc6618a892941bf27b7adf
frantic03117 <shahrukh@franticpro.com>
2025-04-16 19:05:03 +0530

    updated

--- src/controllers/AgoraController.js
+++ src/controllers/AgoraController.js
@@ -2,11 +2,12 @@ const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
 const Booking = require("../models/Booking");
 const APP_ID = "379eda36f4594f8497be07fa4eba6a0c";
 const APP_CERTIFICATE = "8c65f495efbe4096918873f245541ffc";
-exports.getAgoraToken = async () => {
+exports.getAgoraToken = async (req, res) => {
     try {
         const { booking_id } = req.body;
         const channelName = `appointment_${booking_id}`;
         const uid = req.user._id;
+        const role = req.user.role;
         const booking = await Booking.findOne({ _id: booking_id });
         const duration = booking.duration * 60; //seconds
         const currentTimestamp = Math.floor(Date.now() / 1000);
