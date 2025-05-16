0c943392ecfdb84459deffff05622c926cef2b42
frantic03117 <shahrukh@franticpro.com>
2025-05-16 19:00:20 +0530

    updated

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const { Auth } = require("../middleware/Auth");
 const { getOrCreateChatRoom, getMyAllRooms } = require("../controllers/chatRoomController");
-const { get_chat_message } = require("../controllers/ChatController");
+const { get_chat_message, send_chat_message } = require("../controllers/ChatController");
 
 const router = Router();
 router.post('/room', Auth, getOrCreateChatRoom);
 router.get('/room', Auth, getMyAllRooms);
 router.post('/', Auth, get_chat_message);
+router.post('/send', Auth, send_chat_message);
 module.exports = router;
