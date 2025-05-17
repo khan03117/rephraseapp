1667ff56a94760682a1d7038036b4c2148669f6d
frantic03117 <shahrukh@franticpro.com>
2025-05-17 18:15:50 +0530

    updated

--- src/controllers/AgoraController.js
+++ src/controllers/AgoraController.js
@@ -25,12 +25,12 @@ exports.start_meet = async (req, res) => {
     const shouldRegenerateToken =
         !agoraTokenGeneratedAt ||
         (currentTime - new Date(agoraTokenGeneratedAt)) > bookingDuration;
-    if (shouldRegenerateToken) {
-        const uid = req.user._id.toString()
-        const urole = req.user.role;
-        agora_token = await getAgoraToken(booking_id, uid, urole);
-        await Booking.findOneAndUpdate({ _id: bookng._id }, { agora_token: agora_token, agora_token_generated_at: new Date() }, { new: true });
-    }
+
+    const uid = req.user._id.toString()
+    const urole = req.user.role;
+    agora_token = await getAgoraToken(booking_id, uid, urole);
+    await Booking.findOneAndUpdate({ _id: bookng._id }, { agora_token: agora_token, agora_token_generated_at: new Date() }, { new: true });
+
     const updated_bookng = await Booking.findOne(fdata).populate('doctor', "name email mobile profile_image fcm_token").populate('user', "name email mobile profile_image fcm_token");
 
     const bodymessage = req.user.role == "User" ? `Your consultation with ${bookng.doctor.name} has started. Join now to begin your session.` : ` "Consultant ${bookng.user.name} has joined the session. Join now to begin.",`
