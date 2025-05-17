ad27a75abbe923f1c5fb8af0a4b8b95dda094200
frantic03117 <shahrukh@franticpro.com>
2025-05-17 18:15:28 +0530

    updatd

--- src/middleware/Helper.js
+++ src/middleware/Helper.js
@@ -4,7 +4,6 @@ const APP_ID = "379eda36f4594f8497be07fa4eba6a0c";
 const APP_CERTIFICATE = "8c65f495efbe4096918873f245541ffc";
 exports.getAgoraToken = async (booking_id, uid, role) => {
     try {
-        const { booking_id, uid, role } = req.body;
 
         if (!booking_id || !uid || !role) {
             return res.status(400).json({ error: 'Missing parameters' });
@@ -27,15 +26,8 @@ exports.getAgoraToken = async (booking_id, uid, role) => {
             privilegeExpiredTs
         );
 
-        res.json({
-            token,
-            channelName,
-            uid: 0,
-            appId: APP_ID,
-            expiresAt: privilegeExpiredTs,
-        });
+        return token;
     } catch (err) {
         console.error('Token generation error:', err);
-        res.status(500).json({ error: 'Failed to generate token' });
     }
 };
