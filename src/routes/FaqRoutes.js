d84666a3d5339a688bb4a033b393f9c75bc49d98
frantic03117 <shahrukh@franticpro.com>
2025-05-26 15:29:40 +0530

    with auth

--- src/routes/FaqRoutes.js
+++ src/routes/FaqRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { _create, getAll, destroy } = require("../controllers/FaqController");
+const { _create, getAll, destroy, updatefaq } = require("../controllers/FaqController");
 const { Auth } = require("../middleware/Auth");
 
 const router = Router();
 router.get('/', getAll);
-router.post('/', _create);
+router.post('/', Auth, _create);
+router.put('/update/:id', Auth, updatefaq);
 router.delete('/destroy/:id', Auth, destroy);
 module.exports = router;
