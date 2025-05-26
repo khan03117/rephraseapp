9fc96ddbf95e1e36f13ec10afe7fed73f03d500e
frantic03117 <shahrukh@franticpro.com>
2025-05-26 15:29:12 +0530

    deleted

--- src/routes/FaqRoutes.js
+++ src/routes/FaqRoutes.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
-const { _create, getAll } = require("../controllers/FaqController");
+const { _create, getAll, destroy } = require("../controllers/FaqController");
+const { Auth } = require("../middleware/Auth");
 
 const router = Router();
 router.get('/', getAll);
 router.post('/', _create);
+router.delete('/destroy/:id', Auth, destroy);
 module.exports = router;
